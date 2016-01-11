0e882d92332cfda67cf65a81077e840196db1ac8
Tobias Kaatz <tobias@kaatz.eu>
2016-01-11 08:26:16 +0100

    cleaning up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Note: I've written this using sh so it works in the busybox container too
 
 # USE the trap if you need to also do manual cleanup after the service is stopped,
 #     or need to start multiple services in the one container
