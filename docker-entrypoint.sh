da28d68b024107c53690d8cbedbba3d98ec6943f
13yo <tobias.kaatz@googlemail.com>
2016-01-11 14:45:40 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # USE the trap if you need to also do manual cleanup after the service is stopped,
 #     or need to start multiple services in the one container
-trap "echo TRAPed signal" HUP INT QUIT KILL TERM
+# trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
 /opt/$WSO2_ESB_VERSION/bin/wso2server.sh > /tmp/esb.log 2> /tmp/esb_error.log &
 /opt/$WSO2_DSS_VERSION/bin/wso2server.sh > /tmp/dss.log 2> /tmp/dss_error.log &
